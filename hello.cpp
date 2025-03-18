6952d19b7e0456ca4e1b4fee5fe52ab28e9db1dc
Nithin-ganiga <nithinganiga959@gmail.com>
2025-03-18 19:20:42 +0530

    Updated hello.cpp with new changes

--- hello.cpp
+++ hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "I have successfully built and run this  using webhook \n";
+  cout << "I have successfully built and run this  using webhook \n";
   return 0;
 }
